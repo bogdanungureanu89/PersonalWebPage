423af7ead68bcd1d17367ebbcd1e50f584c5804e
Bogdan Ungureanu <45429109+bogdanungureanu89@users.noreply.github.com>
2019-01-20 23:18:25 +0200

    Develop (#2)
    * Added header and footer components
    
    * Added routes and their respective components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
